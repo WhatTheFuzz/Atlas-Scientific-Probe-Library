988140a2f3ed413d5d77678f82b0d369a275ab82
Sean Deaton <sean.deaton@me.com>
2023-04-14 20:35:56 -0400

    Add get_baud.

--- atlasscientific/ftdi/phprobe.py
+++ atlasscientific/ftdi/phprobe.py
@@ -1,6 +1,7 @@
 from atlasscientific.ftdi.atlasdevice import AtlasDevice
 from time import sleep
 from enum import Enum
+import re
 
 class PH_Commands(Enum):
     READ: str = 'r'
@@ -31,6 +32,21 @@ class PHProbe(AtlasDevice):
             print(f'[-] Failed to read pH, got string {line}; expected float.')
 
     
+    def set_baud(self, rate: int=9600) -> bool:
+        '''Change the baud rate of the device.'''
+        pass
+
+    def get_baud(self) -> int:
+        self.flush()
+        self.send_cmd('baud,?')
+        self.wait()
+        line = self.read_line()
+        try:
+            baud = re.search(r'\d+', line).group(0)
+            return baud
+        except IndexError:
+            raise ValueError("Unable to get baud, expected int, got {line}")
+
 if __name__ == '__main__':
     ph: PHProbe = PHProbe(device_id="DK0G4FXK")
-    print(ph.read_ph())
+    print(ph.get_baud())
