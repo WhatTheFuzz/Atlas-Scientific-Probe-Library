269d8e9a460cb57db28b0b9ad25e0088b96f9a60
Sean Deaton <sean.deaton@me.com>
2023-04-19 16:40:28 -0400

    Fix pylint errors.

--- atlasscientific/ftdi/atlasdevice.py
+++ atlasscientific/ftdi/atlasdevice.py
@@ -1,13 +1,16 @@
-import string
+'''A generic Atlas Scientific device class that uses pylibftdi to communicate
+with the sensor. This file is not meant to be used directly, but rather to act
+as the superclass that other, more specific sensor classes inherit from.
+'''
 
-import pylibftdi
+import os
+from time import sleep
 from pylibftdi.device import Device
 from pylibftdi.driver import FtdiError
 from pylibftdi import Driver
-import os
-import time
 
 class AtlasDevice(Device):
+    '''A generic Atlas Scientific device.'''
 
     def __init__(self, device_id: str) -> None:
         super().__init__(mode='t', device_id=device_id)
@@ -16,6 +19,7 @@ class AtlasDevice(Device):
 
 
     def wait(self) -> None:
+        '''Wait for the recommended reading delay for the sensor.'''
         sleep(self.wait_time)
 
     def read_line(self, size=0):
@@ -24,7 +28,7 @@ class AtlasDevice(Device):
         and call this class' super method.
         """
         os.linesep = '\r'
-        return self.readline()
+        return self.readline(size=size)
 
     def read_lines(self):
         """
@@ -50,101 +54,18 @@ class AtlasDevice(Device):
         except FtdiError:
             print("Failed to send command to the sensor.")
             return 0
-            
-            
+
 def get_ftdi_device_list():
     """
     return a list of lines, each a colon-separated
     vendor:product:serial summary of detected devices
     """
     dev_list = []
-    
     for device in Driver().list_devices():
-        vendor, product, serial = device
+        _, _, serial = device
         dev_list.append(serial)
     return dev_list
 
-
-def main():
-
-    real_raw_input = vars(__builtins__).get('raw_input', input) # used to find the correct function for python2/3
-
-    print("\nWelcome to the Atlas Scientific Raspberry Pi FTDI Serial example.\n")
-    print("    Any commands entered are passed to the board via UART except:")
-    print("    Poll,xx.x command continuously polls the board every xx.x seconds")
-    print("    Pressing ctrl-c will stop the polling\n")
-    print("    Press enter to receive all data in buffer (for continuous mode) \n")
-    print("Discovered FTDI serial numbers:")
-
-    devices = get_ftdi_device_list()
-    cnt_all = len(devices)
-    
-    #print "\nIndex:\tSerial: "
-    for i in range(cnt_all):
-        print(  "\nIndex: ", i, " Serial: ", devices[i])
-    print( "===================================")
-
-    index = 0
-    while True:
-        index = real_raw_input("Please select a device index: ")
-
-        try:
-            dev = AtlasDevice(devices[int(index)])
-            break
-        except pylibftdi.FtdiError as e:
-            print( "Error, ", e)
-            print( "Please input a valid index")
-
-    print( "")
-    print(">> Opened device ", devices[int(index)])
-    print(">> Any commands entered are passed to the board via FTDI:")
-
-    time.sleep(1)
-    dev.flush()
-    
-    
-    while True:
-        input_val = real_raw_input("Enter command: ")
-
-        
-        
-        # continuous polling command automatically polls the board
-        if input_val.upper().startswith("POLL"):
-            delaytime = float(string.split(input_val, ',')[1])
-            
-            dev.send_cmd("C,0") # turn off continuous mode
-            #clear all previous data
-            time.sleep(1)
-            dev.flush()
-            
-            # get the information of the board you're polling
-            print("Polling sensor every %0.2f seconds, press ctrl-c to stop polling" % delaytime)
-    
-            try:
-                while True:
-                    dev.send_cmd("R")
-                    lines = dev.read_lines()
-                    for i in range(len(lines)):
-                        # print lines[i]
-                        if lines[i][0] != '*':
-                            print("Response: " , lines[i])
-                    time.sleep(delaytime)
-    
-            except KeyboardInterrupt:       # catches the ctrl-c command, which breaks the loop above
-                print("Continuous polling stopped")
-
-        else:
-            # pass commands straight to board
-            if len(input_val) == 0:
-                lines = dev.read_lines()
-                for i in range(len(lines)):
-                    print( lines[i])
-            else:
-                dev.send_cmd(input_val)
-                time.sleep(1.3)
-                lines = dev.read_lines()
-                for i in range(len(lines)):
-                    print( lines[i])
 
 if __name__ == '__main__':
     pass
--- atlasscientific/ftdi/phprobe.py
+++ atlasscientific/ftdi/phprobe.py
@@ -1,46 +1,43 @@
-from atlasscientific.ftdi.atlasdevice import AtlasDevice
-from time import sleep
-from enum import Enum
-import re
+'''A class representing the Atlas Scientific pH probe.'''
 
-class PH_Commands(Enum):
-    READ: str = 'r'
+import re
+from atlasscientific.ftdi.atlasdevice import AtlasDevice
+
 
 class PHProbe(AtlasDevice):
+    '''Stubs out the Atlas Scientific pH probe.'''
 
     def __init__(self, device_id: str) -> None:
         super().__init__(device_id=device_id)
-
-    def wait(self) -> None:
-        sleep(1)
 
     def read_ph(self) -> float:
         '''Sends a command to read the pH and waits for the response.
         Returns a floating point number of the detected pH.
         '''
         # Send the 'r' command for a single read.
-        self.send_cmd(PH_Commands.READ.value)
+        self.send_cmd('r')
         # Read the line.
         line: str = self.read_line()
         try:
             return float(line)
-        except ValueError as e:
-            print(f'[-] Failed to read pH, got string {line}; expected float.')
+        except ValueError as err:
+            raise ValueError(f'[-] Failed to read pH, got string {line}; expected float.') from err
 
-    
+
     def set_baud(self, rate: int=9600) -> bool:
         '''Change the baud rate of the device.'''
         pass
 
     def get_baud(self) -> int:
+        '''Get the current baud rate of the device.'''
         self.flush()
         self.send_cmd('baud,?')
         line = self.read_line()
         try:
             baud = re.search(r'\d+', line).group(0)
             return baud
-        except IndexError:
-            raise ValueError("Unable to get baud, expected int, got {line}")
+        except IndexError as err:
+            raise ValueError("Unable to get baud, expected int, got {line}") from err
 
 if __name__ == '__main__':
     ph: PHProbe = PHProbe(device_id="DK0G4FXK")
